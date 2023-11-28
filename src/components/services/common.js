ed61adea56229cc7e0792bb204cd5539193a6c5d
Mohan Pandit <mohanpandit131@gmail.com>
2023-11-28 18:36:40 +0530

    origins

--- src/components/services/common.js
+++ src/components/services/common.js
@@ -1,15 +1,15 @@
 
 
-export const ApiURL ="http://127.0.0.1:8005"
+// export const ApiURL ="http://127.0.0.1:8005"
 
-export const uptask_url = "http://127.0.0.1:8000"
-export const socket_url = "ws://127.0.0.1:8005"
-
-
+// export const uptask_url = "http://127.0.0.1:8000"
+// export const socket_url = "ws://127.0.0.1:8005"
 
 
-// export const ApiURL ="http://13.232.79.206:8000"
-// export const uptask_url = "https://uptaskdemo.visiontrek.in"
-// export const socket_url = "ws://13.232.79.206:8000"
+
+
+export const ApiURL ="http://13.232.79.206:8005"
+export const uptask_url = "https://uptaskdemo.visiontrek.in"
+export const socket_url = "ws://13.232.79.206:8005"
 
 
